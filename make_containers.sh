e159a413790331d44521f0061c06f9fd4e7c921a
Andrew Fasano <fasano@mit.edu>
2023-10-11 15:29:37 -0400

    Bugfixes for finding extracted root directory

--- make_containers.sh
+++ make_containers.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+rm -f unblob*.sif
+
 echo "1: Building docker container"
-docker build -t myunblob .
+docker build -t unblob .
 
 echo "2: Converting to singularity"
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/output --privileged -t --rm quay.io/singularity/docker2singularity myunblob
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/output --privileged -t --rm quay.io/singularity/docker2singularity unblob
+
+mv unblob*.sif unblob.sif
--- run_inner.sh
+++ run_inner.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-set -e
+set -eu
 
-unblob $@
+# USAGE ./run_inner.sh /INPUT/FW /SCRATCH/DIR /OUT/BASENAME
+INFILE=$1     # /firmware/dir/vendor/blob.bin
+SCRATCHDIR=$2 # /tmp
+OUTBASE=$3    # /vendor/blob.bin (e.g., outputs written into /data/output/vendor/blob.bin.tar.gz)
+shift 3
 
 # Newest file with a name that unblob could've made
 #MOST_RECENT_FILE=$(find . -name "*_extract*" -type d -printf "%T@ %p\n" | sort -nr | awk '{print $2}' | head -n1)
 set -o pipefail
-MOST_RECENT_FILE="$(basename "${1}_extract")" || (echo "Failed to take basename of \"${1}\""; exit 1)
+EXTRACTNAME="$(basename "${INFILE}_extract")" || (echo "Failed to take basename of \"${1}\""; exit 1)
 set +o pipefail
 
-if [[ ! -d "$MOST_RECENT_FILE" ]]; then
-  echo "Missing extraction ${1}_extract"
-  exit 1
-fi
+unblob --extract-dir="${SCRATCHDIR}" "$INFILE"
 
 # Search in there for the rootfs
-POTENTIAL_DIRS=$(find $MOST_RECENT_FILE -type d \( -name "bin" -o -name "boot" -o -name "dev" -name "etc" -o -name "home" -o -name "lib" -o -name "media" -o -name "mnt" -o -name "opt" -o -name "proc" -o -name "root" -o -name "sbin" -o -name "sys" -o -name "tmp" -o -name "usr" -o -name "var" \) -exec dirname {} \; | sort | uniq -c |  awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- | sort -rg)
+POTENTIAL_DIRS=$(find "${SCRATCHDIR}/"*_extract -type d \( -name "bin" -o -name "boot" -o -name "dev" -name "etc" -o -name "home" -o -name "lib" -o -name "media" -o -name "mnt" -o -name "opt" -o -name "proc" -o -name "root" -o -name "sbin" -o -name "sys" -o -name "tmp" -o -name "usr" -o -name "var" \) -exec dirname {} \; | sort | uniq -c |  awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- | sort -rg)
 
 # If we found at least one, let's grab it
 if [[ ! -z "${POTENTIAL_DIRS}" ]]; then
@@ -26,9 +27,7 @@ if [[ ! -z "${POTENTIAL_DIRS}" ]]; then
 	FIRST_ROOT="$(echo "$FIRST_DIR" | xargs echo -n | cut -d ' ' -f 2-)" # This is gross. Trim leading whitespace with xargs, then take everything after first space
 
 	echo "Selecting $FIRST_ROOT as it matched $FIRST_COUNT critera"
-	ROOT_DIR=$FIRST_ROOT
-
-	tar cfz /data/output/${1}.tar.gz -C ${ROOT_DIR} .
+	tar cfz "/data/output/${OUTBASE}.tar.gz" -C "${FIRST_ROOT}" .
 
 else
 	echo "FAILURE: no root directory found"
