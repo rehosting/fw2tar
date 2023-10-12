50f760e34b17293d3c0b3aa01a7efa14458ed16f
Andrew Fasano <fasano@mit.edu>
2023-10-12 09:39:21 -0400

    Unique logfiles, bugfix for tar compression path

--- run_inner.sh
+++ run_inner.sh
@@ -14,7 +14,7 @@ set -o pipefail
 EXTRACTNAME="$(basename "${INFILE}_extract")" || (echo "Failed to take basename of \"${1}\""; exit 1)
 set +o pipefail
 
-unblob --extract-dir="${SCRATCHDIR}" "$INFILE"
+unblob --log="/data/output/${OUTBASE}_log.txt" --extract-dir="${SCRATCHDIR}" "$INFILE"
 
 # Search in there for the rootfs
 POTENTIAL_DIRS=$(find "${SCRATCHDIR}/"*_extract -type d \( -name "bin" -o -name "boot" -o -name "dev" -name "etc" -o -name "home" -o -name "lib" -o -name "media" -o -name "mnt" -o -name "opt" -o -name "proc" -o -name "root" -o -name "sbin" -o -name "sys" -o -name "tmp" -o -name "usr" -o -name "var" \) -exec dirname {} \; | sort | uniq -c |  awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- | sort -rg)
@@ -26,8 +26,8 @@ if [[ ! -z "${POTENTIAL_DIRS}" ]]; then
 	FIRST_COUNT=$(echo "$FIRST_DIR" | awk '{print $1}')
 	FIRST_ROOT="$(echo "$FIRST_DIR" | xargs echo -n | cut -d ' ' -f 2-)" # This is gross. Trim leading whitespace with xargs, then take everything after first space
 
-	echo "Selecting $FIRST_ROOT as it matched $FIRST_COUNT critera. Writing out to /data/output/${OUTBASE}.tar.gz"
-	tar cfz -dev "/data/output/${OUTBASE}.tar.gz" -C "${FIRST_ROOT}" .
+	#echo "Selecting $FIRST_ROOT as it matched $FIRST_COUNT critera. Writing out to /data/output/${OUTBASE}.tar.gz"
+	tar czf "/data/output/${OUTBASE}.tar.gz" --xattrs -C "${FIRST_ROOT}" .
 
 else
 	echo "FAILURE: no root directory found"
