6178719cfe48d103fd73e96d141d43061e0c7274
Andrew Fasano <fasano@mit.edu>
2024-02-07 15:54:23 -0500

    Improve rootfs detection logic (fixes significant differences between two extractors)

--- run_binwalk.sh
+++ run_binwalk.sh
@@ -21,19 +21,27 @@ LOGBASE="${INFILE%.*}"
 mkdir -p "${SCRATCHDIR}/binwalk_initial"
 binwalk --run-as=root --preserve-symlinks -eM --log="${LOGBASE}.unblob.txt" -q "$INFILE" -C "${SCRATCHDIR}/binwalk_initial"
 
-# Search in there for the rootfs
-POTENTIAL_DIRS=$(find "${SCRATCHDIR}/binwalk_initial/" -type d \( -name "bin" -o -name "boot" -o -name "dev" -name "etc" -o -name "home" -o -name "lib" -o -name "media" -o -name "mnt" -o -name "opt" -o -name "proc" -o -name "root" -o -name "sbin" -o -name "sys" -o -name "tmp" -o -name "usr" -o -name "var" \) -exec dirname {} \; | sort | uniq -c |  awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- | sort -rg)
+EXTRACT_DIR="${SCRATCHDIR}/binwalk_initial/"
 
-# If we found at least one, let's grab it
+# Find directories of interest, count occurrences, and prepare for sorting
+POTENTIAL_DIRS=$(find "${EXTRACT_DIR}" -type d \( -name "bin" -o -name "boot" -o -name "dev" -o -name "etc" -o -name "home" -o -name "lib" -o -name "media" -o -name "mnt" -o -name "opt" -o -name "proc" -o -name "root" -o -name "sbin" -o -name "sys" -o -name "tmp" -o -name "usr" -o -name "var" \) \
+| while read dirPath; do
+    if [ "$(find "${dirPath}" -mindepth 1 -maxdepth 1 -type f | wc -l)" -gt 0 ]; then
+        parentDir=$(dirname "${dirPath}")
+        depth=$(echo "${parentDir}" | grep -o "/" | wc -l)
+        size=$(du -s "${parentDir}" | cut -f1)
+        echo "${size} ${depth} ${parentDir}"
+    fi
+done | sort -k1,1nr -k2,2n | head -n 1 | cut -d' ' -f3-)
+
+# Check if we found at least one potential directory
 if [[ -z "${POTENTIAL_DIRS}" ]]; then
-	echo "FAILURE: no root directory found"
-	exit 1
+    echo "FAILURE: no root directory found"
+    exit 1
 fi
-
-# count dirname. Let's just grab the most likely
-FIRST_DIR=$(echo -e "$POTENTIAL_DIRS" | head -n1)
-FIRST_COUNT=$(echo "$FIRST_DIR" | awk '{print $1}')
-FIRST_ROOT="$(echo "$FIRST_DIR" | xargs echo -n | cut -d ' ' -f 2-)" # This is gross. Trim leading whitespace with xargs, then take everything after first space
+# Extract the most likely root directory, now simply the first line of POTENTIAL_DIRS
+FIRST_ROOT=$(echo -e "$POTENTIAL_DIRS" | head -n1)
+echo "First root is $FIRST_ROOT"
 
 # Now do 2nd pass to just get rootfs without extra extractions
 REL_PATH=$(echo "$FIRST_ROOT" | sed "s|${SCRATCHDIR}/binwalk_initial||g")
