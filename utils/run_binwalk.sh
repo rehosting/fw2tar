c406ef834aa1de1fb230cc2f8237cb335ef27fe8
Andrew Fasano <fasano@mit.edu>
2024-02-18 19:10:18 -0500

    Add run unblob/binwalk utilities for debugging

---
+++ utils/run_binwalk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eu
+
+if [ $# -lt 1 ]; then
+  exit 1
+fi
+
+if [ ! -f "$1" ]; then
+  echo "Error: Input file not found"
+  exit 1
+fi
+
+# Resolve argument path and map into container appropriately
+IN_PATH=$(readlink -f "$1")
+IN_DIR=$(dirname "$IN_PATH")
+IN_FILE=$(basename "$IN_PATH")
+
+# Need no entrypoint on dockerfile
+docker run --rm -v ${IN_DIR}:/host fw2tar binwalk -0 root -1 --preserve-symlinks -eM /host/${IN_FILE} -C /host/${IN_FILE}_extract.binwalk
---
+++ utils/run_unblob.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -eu
+
+if [ ! -f "$1" ]; then
+  echo "Error: Input file not found"
+  exit 1
+fi
+
+# Resolve argument path and map into container appropriately
+IN_PATH=$(readlink -f "$1")
+IN_DIR=$(dirname "$IN_PATH")
+IN_FILE=$(basename "$IN_PATH")
+
+if [ -d unblob ]; then
+  echo "Using local unblob"
+  docker run --rm -v ${IN_DIR}:/host -v $(pwd)/unblob:/unblob \
+  fw2tar unblob -v "/host/${IN_FILE}"
+else
+  docker run --rm -v ${IN_DIR}:/host fw2tar \
+  unblob -v "/host/${IN_FILE}"
+fi
