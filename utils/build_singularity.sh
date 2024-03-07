7c15a9484f4f2526d57eb6f42fca110f0ba2a80a
Andrew Fasano <fasano@mit.edu>
2024-03-07 18:47:04 -0500

    move singularity builder into utils

--- utils/build_singularity.sh
+++ utils/build_singularity.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -eu
 
-rm -f fw2tar*.sif || true
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+OUT=$(dirname "$SCRIPTPATH")
+
+
+rm -f $OUT/fw2tar*.sif || true
 
 echo "1: Building docker container"
-docker build -t fw2tar .
+docker build -t fw2tar $OUT
 
 echo "2: Converting to singularity"
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
@@ -12,4 +16,4 @@ docker run -v /var/run/docker.sock:/var/run/docker.sock \
     --privileged  -t \
     --rm quay.io/singularity/docker2singularity:v3.9.0 fw2tar
 
-mv fw2tar*.sif fw2tar.sif
+mv fw2tar*.sif $OUT/fw2tar.sif
