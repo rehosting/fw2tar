85b371efa7dba8aa5f01732db6782130294c3aec
jamcleod <Jordan.McLeod@ll.mit.edu>
2024-05-14 13:39:43 -0400

    Add test for Mikrotik RB750GR3

--- tests/end_to_end.sh
+++ tests/end_to_end.sh
@@ -43,18 +43,30 @@ test() {
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
 # Download TP-Link AX1800 Firmware
-curl "https://static.tp-link.com/upload/firmware/2023/202308/20230818/Archer%20AX1800(US)_V4.6_230725.zip" \
-    -o /tmp/ax1800_firmware.zip
-
 FIRMWARE_PATH="/tmp/ax1800_firmware.zip"
+
+curl "https://static.tp-link.com/upload/firmware/2023/202308/20230818/Archer%20AX1800(US)_V4.6_230725.zip" \
+    -o "$FIRMWARE_PATH"
+
 FIRMWARE_LISTING="$SCRIPT_DIR/results/ax1800_listing.txt"
 
 test $FIRMWARE_PATH $FIRMWARE_LISTING "AX1800"
 
-# Download NETGEAR AX5400 (RAX54S)
-curl "https://www.downloads.netgear.com/files/GDC/RAX54S/RAX54Sv2-V1.1.4.28.zip" \
-    -o /tmp/rax54s_firmware.zip
-
+# Download NETGEAR AX5400 (RAX54S) firmware
 FIRMWARE_PATH="/tmp/rax54s_firmware.zip"
+
+curl "https://www.downloads.netgear.com/files/GDC/RAX54S/RAX54Sv2-V1.1.4.28.zip" \
+    -o "$FIRMWARE_PATH"
+
 FIRMWARE_LISTING="$SCRIPT_DIR/results/rax54s_listing.txt"
 test $FIRMWARE_PATH $FIRMWARE_LISTING "RAX54S"
+
+# Download Mikrotik RB750Gr3 firmware
+FIRMWARE_PATH="/tmp/rb750gr3_firmware.zip"
+
+curl "https://download.mikrotik.com/routeros/7.14.3/routeros-7.14.3-mmips.npk" \
+    -o "$FIRMWARE_PATH"
+
+FIRMWARE_LISTING="$SCRIPT_DIR/results/rb750gr3_listing.txt"
+
+test $FIRMWARE_PATH $FIRMWARE_LISTING "RB750Gr3"
