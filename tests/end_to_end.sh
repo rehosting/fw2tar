dd77e69b18069e8523c239a036bb47d87b8c8c95
jamcleod <Jordan.McLeod@ll.mit.edu>
2024-05-14 14:07:23 -0400

    Add test for ASUS RT-AX86U Pro

--- tests/end_to_end.sh
+++ tests/end_to_end.sh
@@ -5,6 +5,11 @@ test() {
     FIRMWARE_LISTING=$2
     FIRMWARE_NAME=$3
 
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    YELLOW='\033[0;33m'
+    END='\033[0m'
+
     OLD_FIRMWARE_LISTING="$FIRMWARE_LISTING.old"
     NEW_FIRMWARE_LISTING="$FIRMWARE_LISTING.new"
 
@@ -23,19 +28,19 @@ test() {
     $SCRIPT_DIR/../fw2tar.sh $FIRMWARE_PATH $FIRMWARE_PATH_OUT --force
 
     if ! [ -f "$ROOTFS" ]; then
-        echo "Failed to extract ${FIRMWARE_NAME}"
+        echo -e "${RED}Failed to extract ${FIRMWARE_NAME}${END}"
         exit 1
     fi
 
     tar -tvf "$ROOTFS" | awk '{ print $6 " " $2 " " $1 " " $3 " " $4  }' | column -t | sort > $NEW_FIRMWARE_LISTING
 
     if [[ "$NEW_FIRMWARE_LISTING" == "$OLD_FIRMWARE_LISTING" ]]; then
-        echo "Generated new firmware listing for ${FIRMWARE_NAME}. Nothing to diff, skipping."
+        echo -e "Generated new firmware listing for ${FIRMWARE_NAME}. ${YELLOW}Nothing to diff, skipping.${END}"
     else
         if ! diff --color=always "$OLD_FIRMWARE_LISTING" "$NEW_FIRMWARE_LISTING"; then
-            echo "Listings for ${FIRMWARE_NAME} do not match. To approve changes replace ${OLD_FIRMWARE_LISTING} with ${NEW_FIRMWARE_LISTING}"
+            echo -e "${RED}Listings for ${FIRMWARE_NAME} do not match.${END} To approve changes replace ${OLD_FIRMWARE_LISTING} with ${NEW_FIRMWARE_LISTING}"
         else
-            echo "Firmware listing matches for ${FIRMWARE_NAME}."
+            echo -e "${GREEN}Firmware listing matches for ${FIRMWARE_NAME}.${END}"
         fi
     fi
 }
@@ -62,7 +67,7 @@ FIRMWARE_LISTING="$SCRIPT_DIR/results/rax54s_listing.txt"
 test $FIRMWARE_PATH $FIRMWARE_LISTING "RAX54S"
 
 # Download Mikrotik RB750Gr3 firmware
-FIRMWARE_PATH="/tmp/rb750gr3_firmware.zip"
+FIRMWARE_PATH="/tmp/rb750gr3_firmware.npk"
 
 curl "https://download.mikrotik.com/routeros/7.14.3/routeros-7.14.3-mmips.npk" \
     -o "$FIRMWARE_PATH"
@@ -70,3 +75,14 @@ curl "https://download.mikrotik.com/routeros/7.14.3/routeros-7.14.3-mmips.npk" \
 FIRMWARE_LISTING="$SCRIPT_DIR/results/rb750gr3_listing.txt"
 
 test $FIRMWARE_PATH $FIRMWARE_LISTING "RB750Gr3"
+
+# Download ASUS RT-AX86U Pro firmware
+
+FIRMWARE_PATH="/tmp/ax86u_firmware.zip"
+
+curl "https://dlcdnets.asus.com/pub/ASUS/wireless/RT-AX86U_Pro/FW_RT_AX86U_PRO_300610234312.zip?model=RT-AX86U%20Pro" \
+    -o "$FIRMWARE_PATH"
+
+FIRMWARE_LISTING="$SCRIPT_DIR/results/ax86u_listing.txt"
+
+test $FIRMWARE_PATH $FIRMWARE_LISTING "RT-AX86U Pro"
