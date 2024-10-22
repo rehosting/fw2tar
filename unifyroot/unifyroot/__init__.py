0544c42ed79e4fe7ac3bf082c31f326a0a89f741
Andrew Fasano <fasano@mit.edu>
2024-10-22 15:52:56 -0400

    fix(cli): silence import warning

--- unifyroot/unifyroot/__init__.py
+++ unifyroot/unifyroot/__init__.py
@@ -1,5 +1,4 @@
 from .common import FilesystemInfo, FilesystemRepository, FilesystemLoader
 from .filesystemunifier import FilesystemUnifier
-from .cli import unify_filesystems
 
 __all__ = ['FilesystemInfo', 'FilesystemRepository', 'FilesystemLoader', 'FilesystemUnifier', 'unify_filesystems']
