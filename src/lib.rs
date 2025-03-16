27252d7eeb79621e4f52f698d82a19215329db80
jamcleod <Jordan.McLeod@ll.mit.edu>
2025-03-16 17:09:44 -0400

    Add command line, make functional

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,70 @@
 pub mod analysis;
 pub mod archive;
 pub mod args;
+mod error;
 pub mod extractors;
 pub mod metadata;
+
+use analysis::{extract_and_process, ExtractionResult};
+use metadata::Metadata;
+
+use std::env;
+use std::sync::Mutex;
+use std::thread;
+
+pub use error::Fw2tarError;
+
+pub fn main(args: args::Args) -> Result<(), Fw2tarError> {
+    let metadata = Metadata {
+        input_hash: analysis::sha1_file(&args.firmware).unwrap_or_default(),
+        file: args.firmware.display().to_string(),
+        fw2tar_command: env::args().collect(),
+    };
+
+    let extractors: Vec<_> = args
+        .extractors
+        .map(|extractors| extractors.split(",").map(String::from).collect())
+        .unwrap_or_else(|| {
+            extractors::all_extractor_names()
+                .map(String::from)
+                .collect()
+        });
+
+    let output = args
+        .output
+        .unwrap_or_else(|| args.firmware.with_extension(""));
+
+    let results: Mutex<Vec<ExtractionResult>> = Mutex::new(Vec::new());
+
+    thread::scope(|threads| -> Result<(), Fw2tarError> {
+        for extractor in extractors {
+            let extractor = extractors::get_extractor(&extractor)
+                .ok_or_else(|| Fw2tarError::InvalidExtractor(extractor.clone()))?;
+
+            let firmware = args.firmware.clone();
+            let output = output.clone();
+            let scratch_dir = args.scratch_dir.clone();
+            let results = &results;
+            let metadata = &metadata;
+
+            threads.spawn(move || {
+                extract_and_process(
+                    extractor,
+                    &firmware,
+                    &output,
+                    scratch_dir.as_deref(),
+                    args.loud,
+                    args.primary_limit,
+                    args.secondary_limit,
+                    results,
+                    metadata,
+                )
+                .unwrap();
+            });
+        }
+
+        Ok(())
+    })?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,14 @@
-use fw2tar::analysis::{extract_and_process, ExtractionResult};
-use fw2tar::extractors;
-use fw2tar::metadata::Metadata;
-
-use std::path::Path;
-use std::sync::Mutex;
+use clap::Parser;
+use fw2tar::args::Args;
+use std::process::exit;
 
 fn main() {
+    let args = Args::parse();
+
     pretty_env_logger::init_custom_env("FW2TAR_LOG");
 
-    let results: Mutex<Vec<ExtractionResult>> = Mutex::new(Vec::new());
-
-    extract_and_process(
-        extractors::get_extractor("unblob").unwrap(),
-        Path::new("../temp/fw2tar_test/rv130_archive.tar.gz"),
-        Path::new("./rv130_test"),
-        None,
-        true,
-        1,
-        0,
-        &results,
-        &Metadata {
-            input_hash: "insert input hash".into(),
-            file: "rv130_archive.tar.gz".into(),
-            fw2tar_command: vec!["early".into(), "test".into()],
-        },
-    )
-    .unwrap();
+    if let Err(e) = fw2tar::main(args) {
+        eprintln!("{e}");
+        exit(1);
+    }
 }
