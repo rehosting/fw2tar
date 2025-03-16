a42f6c4c1c1a00e9abd37f2e97e24614455d52f7
jamcleod <Jordan.McLeod@ll.mit.edu>
2025-03-16 00:12:57 -0400

    Finish find_linux_filesystems

--- src/analysis/find_linux_filesystems.rs
+++ src/analysis/find_linux_filesystems.rs
@@ -1,5 +1,8 @@
+use std::cmp::Reverse;
 use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
+
+use super::directory_executables::{get_dir_executable_info, ExecutableInfo};
 
 const DEFAULT_MIN_EXECUTABLES: usize = 10;
 const MAX_EXPLORE_DEPTH: usize = 15;
@@ -9,11 +12,21 @@ pub const CRITICAL_FILES: &[&str] = &["bin/sh", "etc/passwd"];
 
 const MIN_REQUIRED: usize = (KEY_DIRS.len() + CRITICAL_FILES.len()) / 2;
 
+pub struct PrimaryFilesystem {
+    path: PathBuf,
+    size: u64,
+    num_files: usize,
+    key_file_count: usize,
+    executables: usize,
+}
+
 pub fn find_linux_filesystems(
     start_dir: &Path,
     min_executables: Option<usize>,
     verbose: bool,
-) -> Vec<(PathBuf, usize, usize)> {
+    extractor_name: &str,
+) -> Vec<PrimaryFilesystem> {
+    let mut filesystems = Vec::new();
     let min_executables = min_executables.unwrap_or(DEFAULT_MIN_EXECUTABLES);
 
     for entry in WalkDir::new(start_dir)
@@ -39,11 +52,29 @@ pub fn find_linux_filesystems(
         }
 
         if total_matches >= MIN_REQUIRED {
-            // TODO
+            let ExecutableInfo {
+                total_executables,
+                total_size,
+                total_files,
+            } = get_dir_executable_info(root);
+
+            if total_executables >= min_executables {
+                filesystems.push(PrimaryFilesystem {
+                    path: root.to_owned(),
+                    size: total_size,
+                    num_files: total_files,
+                    key_file_count: total_matches,
+                    executables: total_executables,
+                })
+            } else {
+                log::warn!("Extractor {extractor_name} did not find enough executables ({total_executables} < {min_executables})")
+            }
         }
     }
 
-    todo!()
+    filesystems.sort_by_key(|fs| Reverse((fs.executables, fs.size, fs.key_file_count)));
+
+    filesystems
 }
 
 #[cfg(test)]
